7f3399d52ccc3679abac1368d7ebae35ffb8e904
Lucas Assis <lukas.ecomp@gmail.com>
2019-03-18 21:26:40 -0300

    Add Example Module

--- laravel_boilerplate/webpack.mix.js
+++ laravel_boilerplate/webpack.mix.js
@@ -1,5 +1,10 @@
 const mix = require('laravel-mix');
 
+/* Allow multiple Laravel Mix applications*/
+require('laravel-mix-merge-manifest');
+mix.mergeManifest();
+/*----------------------------------------*/
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
